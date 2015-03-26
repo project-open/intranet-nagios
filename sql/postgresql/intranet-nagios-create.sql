59a0864b9bb516a7036343472d670152c488054e
Frank Bergmann <frank.bergmann@project-open.com>
2015-03-26 16:33:21 +0000

    - Corrected creation script

--- sql/postgresql/intranet-nagios-create.sql
+++ sql/postgresql/intranet-nagios-create.sql
@@ -36,7 +36,7 @@ SELECT im_category_hierarchy_new('HTTP-Server', 'Process', 'Intranet Conf Item T
 -- for various groups who whould be able to see the menu.
 
 create or replace function inline_0 ()
-returns integer as '
+returns integer as $$
 declare
 	-- Menu IDs
 	v_menu			integer;
@@ -52,45 +52,45 @@ declare
 	v_admins		integer;
 BEGIN
 	-- Get some group IDs
-	select group_id into v_admins from groups where group_name = ''P/O Admins'';
-	select group_id into v_senman from groups where group_name = ''Senior Managers'';
-	select group_id into v_proman from groups where group_name = ''Project Managers'';
-	select group_id into v_accounting from groups where group_name = ''Accounting'';
-	select group_id into v_employees from groups where group_name = ''Employees'';
-	select group_id into v_companies from groups where group_name = ''Customers'';
-	select group_id into v_freelancers from groups where group_name = ''Freelancers'';
+	select group_id into v_admins from groups where group_name = 'P/O Admins';
+	select group_id into v_senman from groups where group_name = 'Senior Managers';
+	select group_id into v_proman from groups where group_name = 'Project Managers';
+	select group_id into v_accounting from groups where group_name = 'Accounting';
+	select group_id into v_employees from groups where group_name = 'Employees';
+	select group_id into v_companies from groups where group_name = 'Customers';
+	select group_id into v_freelancers from groups where group_name = 'Freelancers';
 
 	-- Determine the main menu. "Label" is used to
 	-- identify menus.
 	select menu_id into v_main_menu
-	from im_menus where label=''conf_items'';
+	from im_menus where label='conf_items';
 
 	-- Create the menu.
 	v_menu := im_menu__new (
 		null,			-- p_menu_id
-		''acs_object'',		-- object_type
+		'im_menu',		-- object_type
 		now(),			-- creation_date
 		null,			-- creation_user
 		null,			-- creation_ip
 		null,			-- context_id
-		''intranet-nagios'',	-- package_name
-		''nagios'',		-- label
-		''Nagios Main Page'',	-- name
-		''/intranet-nagios/'',	-- url
+		'intranet-nagios',	-- package_name
+		'nagios',		-- label
+		'Nagios Main Page',	-- name
+		'/intranet-nagios/',	-- url
 		85,			-- sort_order
 		v_main_menu,		-- parent_menu_id
 		null			-- p_visible_tcl
 	);
 
 	-- Grant read permissions to most of the system
-	PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_proman, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_employees, ''read'');
+	PERFORM acs_permission__grant_permission(v_menu, v_admins, 'read');
+	PERFORM acs_permission__grant_permission(v_menu, v_senman, 'read');
+	PERFORM acs_permission__grant_permission(v_menu, v_proman, 'read');
+	PERFORM acs_permission__grant_permission(v_menu, v_accounting, 'read');
+	PERFORM acs_permission__grant_permission(v_menu, v_employees, 'read');
 
 	return 0;
-end;' language 'plpgsql';
+end;$$ language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
 
@@ -98,7 +98,7 @@ drop function inline_0 ();
 
 
 create or replace function inline_0 ()
-returns integer as '
+returns integer as $$
 declare
 	-- Menu IDs
 	v_menu			integer;
@@ -114,45 +114,45 @@ declare
 	v_admins		integer;
 BEGIN
 	-- Get some group IDs
-	select group_id into v_admins from groups where group_name = ''P/O Admins'';
-	select group_id into v_senman from groups where group_name = ''Senior Managers'';
-	select group_id into v_proman from groups where group_name = ''Project Managers'';
-	select group_id into v_accounting from groups where group_name = ''Accounting'';
-	select group_id into v_employees from groups where group_name = ''Employees'';
-	select group_id into v_companies from groups where group_name = ''Customers'';
-	select group_id into v_freelancers from groups where group_name = ''Freelancers'';
+	select group_id into v_admins from groups where group_name = 'P/O Admins';
+	select group_id into v_senman from groups where group_name = 'Senior Managers';
+	select group_id into v_proman from groups where group_name = 'Project Managers';
+	select group_id into v_accounting from groups where group_name = 'Accounting';
+	select group_id into v_employees from groups where group_name = 'Employees';
+	select group_id into v_companies from groups where group_name = 'Customers';
+	select group_id into v_freelancers from groups where group_name = 'Freelancers';
 
 	-- Determine the main menu. "Label" is used to
 	-- identify menus.
 	select menu_id into v_main_menu
-	from im_menus where label=''conf_items'';
+	from im_menus where label='conf_items';
 
 	-- Create the menu.
 	v_menu := im_menu__new (
 		null,			-- p_menu_id
-		''acs_object'',		-- object_type
+		'im_menu',		-- object_type
 		now(),			-- creation_date
 		null,			-- creation_user
 		null,			-- creation_ip
 		null,			-- context_id
-		''intranet-nagios'',	-- package_name
-		''nagios_import_conf'',	-- label
-		''Import Nagios Definitions'',	-- name
-		''/intranet-nagios/import-nagios-confitems'',	-- url
+		'intranet-nagios',	-- package_name
+		'nagios_import_conf',	-- label
+		'Import Nagios Definitions',	-- name
+		'/intranet-nagios/import-nagios-confitems',	-- url
 		15,			-- sort_order
 		v_main_menu,		-- parent_menu_id
 		null			-- p_visible_tcl
 	);
 
 	-- Grant read permissions to most of the system
-	PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_proman, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_employees, ''read'');
+	PERFORM acs_permission__grant_permission(v_menu, v_admins, 'read');
+	PERFORM acs_permission__grant_permission(v_menu, v_senman, 'read');
+	PERFORM acs_permission__grant_permission(v_menu, v_proman, 'read');
+	PERFORM acs_permission__grant_permission(v_menu, v_accounting, 'read');
+	PERFORM acs_permission__grant_permission(v_menu, v_employees, 'read');
 
 	return 0;
-end;' language 'plpgsql';
+end;$$ language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
 
